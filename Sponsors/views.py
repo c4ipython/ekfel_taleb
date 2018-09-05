48d02bf24c3cbe2344ca46474e8b93f270261c87
Rand <rand.mustaffa@codeforiraq.org>
2018-09-05 16:58:51 +0300

    [views.py] make sponsor request view in views.py file as (sponserRequest) view

--- Sponsors/views.py
+++ Sponsors/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from Students.models import Req_st
+from django.http import HttpResponse
 # Create your views here.
+
+def sponserRequest(request,id):
+    if request.method == 'POST':
+        e = Req_st.objects.get(id=id)
+        e.req_spon = request.user.username
+        e.save()
+        return HttpResponse('donneeee')
+    else:
+        e = Req_st.objects.get(id=id)
+    return render(request, 'req.html')
