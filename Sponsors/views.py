2bac45f581c65b9ca7c192f5e0e503ea820f4f0c
Rand <rand.mustaffa@codeforiraq.org>
2018-09-05 18:25:22 +0300

    [views.py] 1-add (requires_csrf_token) for security.  2- add sponsor delete view as(sponserdelete) in views.py file.

--- Sponsors/views.py
+++ Sponsors/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from Students.models import Req_st
 from django.http import HttpResponse
+from django.views.decorators.csrf import requires_csrf_token
 # Create your views here.
-
+@requires_csrf_token
 def sponserRequest(request,id):
     if request.method == 'POST':
         e = Req_st.objects.get(id=id)
@@ -11,4 +12,15 @@ def sponserRequest(request,id):
         return HttpResponse('donneeee')
     else:
         e = Req_st.objects.get(id=id)
+    return render(request, 'req.html')
+
+@requires_csrf_token
+def sponserdelete(request,id):
+    if request.method == 'POST':
+        d = Req_st.objects.get(id=id)
+        d.sponser = ""
+        d.save()
+        return HttpResponse('donneeee')
+    else:
+        d = Req_st.objects.get(id=id)
     return render(request, 'req.html')
