d84ec80de4d9e197d5e19c52aa2b019d3fca04c7
Rand <rand.mustaffa@codeforiraq.org>
2018-09-05 16:54:21 +0300

    [urls.py] add Sponsers app to urls.py files

--- akfeltaleb/urls.py
+++ akfeltaleb/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from Sponsors import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path ('',include('home.urls')),
     path('',include('Students.urls')),
+    path('',include('Sponsors.url')),
 
 
 ]
