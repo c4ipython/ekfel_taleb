8292a366fecd1796d28c8161f14eeb46847384eb
Rand <rand.mustaffa@codeforiraq.org>
2018-09-06 23:40:00 +0300

    [url.py]fix some error.

--- Sponsors/url.py
+++ Sponsors/url.py
@@ -5,7 +5,7 @@ from Sponsors import views
 
 urlpatterns = [
 
-path('6alab/<int:id>', views.sponserRequest, name='6alab'),
+path('kafalat/<int:id>', views.sponserRequest, name='6alab'),
 path('delete/<int:id>',views.sponserdelete, name='delete'),
 path('kafalat/', views.displayKafalat, name='kafalat'),
 path('kafalaty/', views.displayMyKafalat, name='kafalaty'),
