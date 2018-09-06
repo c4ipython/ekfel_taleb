3bd9f4f15b2f5a91bbb40b249f546e287a8dd7e9
sherinali <seagull_jonthan@yahoo.co.uk>
2018-09-06 21:59:51 +0300

    add urls for devteam & c4i pages in home app urls

--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,13 @@
-from django.contrib import admin
+
 from django.urls import path
 
 from home import views
 
 urlpatterns = [
-    path ('home/',views.home,name='home'),
+    path ('',views.home,name='home'),
     path ('contact/',views.contact,name='contact'),
     path ('about/',views.about,name='about'),
+    path ('c4i/', views.c4i, name='c4i'),
+    path ('devteam/',views.devteam,name='devteam'),
 
 ]
