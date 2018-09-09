13a3e11b91d70ac98f8cc4cfa939d7c750d37ab0
zainab imad <zainabimad71@gmail.com>
2018-09-09 17:37:43 +0300

    working  the view and html  of the add_request

--- Students/urls.py
+++ Students/urls.py
@@ -2,6 +2,12 @@
 from django.urls import path
 from Students import views
 urlpatterns=[
+         path('Index/',views.index,name='Ind'),
+path('add_re/',views.add_req,name='add_req'),
+
+
+
+
 
 
 ]
--- Students/views.py
+++ Students/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from Students.models import Req_st
+from django.views.decorators.csrf import requires_csrf_token
+from django.shortcuts import redirect
 
-# Create your views here.
+
+def index(request):
+    return render(request,'Index.html')
+
+
+
+# def auths(request):
+#     z=0
+#     st=Student.objects.filter(username=request.user)
+#     sp=Sponsor.objects.filter(username=request.user)
+#     for i in st :
+#         if i.types=='طالب':
+#           z=1
+#       else:
+#           z=0
+#    for i in sp:
+#        if i.types=='كفيل':
+#            z=2
+#      else:
+#          z=0
+# return z
+#
+
+
+@requires_csrf_token
+def add_req(request):
+    # z=auths(request)
+    # if z==1:
+    #     ss=''
+    if request.method=='POST':
+        title=request.POST.get('title')
+        info=request.POST.get('info')
+        forms=Req_st(title=title,info=info,sender=request.user)
+        forms.save()
+        return HttpResponse('ok')
+    else:
+        redirect('Index.html')
+    return render(request,"add_re.html")
