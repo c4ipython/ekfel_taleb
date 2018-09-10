33ed6332ccd67e7be058802c80090deb01e3672c
khetamedan <khetam.edan@codeforiraq.org>
2018-09-10 22:52:42 +0300

    عرض الانشطة حسب التواريخ واضافة زر الحذف وكذلك امكانيات الادمن

--- events/urls.py
+++ events/urls.py
@@ -4,5 +4,8 @@ from events import views
 
 urlpatterns = [
     path ('add/',views.add,name='add_event'),
+    path ('list/',views.viewEvents,name='viewEvents'),
+    path ('list/<int:id>',views.deleted,name='delteEvents'),
+
 
 ]
--- events/views.py
+++ events/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from . import models
+from .models import Evnts
 from django.http import HttpResponse
+from datetime import date,timedelta
+from django.views.decorators.csrf import requires_csrf_token
 
 
 def add(request):
@@ -11,8 +13,29 @@ def add(request):
         source=request.POST.get("source")
         dates=request.POST.get("dates")
         user = request.user
-        form = models.Evnts(title=title, details= details, address=address, source=source, dates= dates, username= user)
+        form = Evnts(title=title, details= details, address=address, source=source, dates= dates, username= user)
         form.save()
         return HttpResponse("good")
     else:
         return render(request,"add.html")
+
+
+def viewEvents(request):
+    yesterday = date.today() - timedelta(hours=25)
+    nextyear = date.today() + timedelta(hours=7200)
+    lastyear = date.today() - timedelta(hours=7200)
+    lis =Evnts.objects.filter(dates__range=(date.today(),nextyear), disabled= False)
+    missed = Evnts.objects.filter(dates__range=(lastyear,yesterday),disabled= False)
+    return render(request, 'viewevents.html',{'list':lis, 'missed':missed})
+
+
+@requires_csrf_token
+def deleted(request,id):
+    if request.method == 'POST':
+        idd = Evnts.objects.get(id=id)
+        idd.disabled= True
+        idd.save()
+        return HttpResponse('deleted')
+    else:
+        idd = Evnts.objects.get(id=id)
+    return render(request, 'dltevent.html',)
