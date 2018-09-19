ae99c104fa72df805545acf2f7617efde6a3c215
alidess <oscarali.info@gmail.com>
2018-09-19 19:00:25 +0300

    تم اكمال صلاحيات لتطبيق الحسابات

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from .models import Sponsor
 from .models import Students
 from django.contrib.auth import login, authenticate, logout
@@ -7,78 +8,11 @@ from django.views.decorators.csrf import requires_csrf_token
 # Create your views here.
 
 
-@requires_csrf_token
-def signup_sponsor(request):
-    if request.method == 'POST':
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        fullName = request.POST.get("fullName")
-        age = request.POST.get("age")
-        birthDate = request.POST.get("birthDate")
-        governorate = request.POST.get("governorate")
-        job = request.POST.get("job")
-        jobAddress = request.POST.get("jobAddress")
-        phone = request.POST.get("phone")
-        img = request.POST.get("img")
-        monthlySalary = request.POST.get("monthlySalary")
-        try:
-            form1 = User.objects.create_user(username=username, password=password)
-            form1.save()
-            form2 = Sponsor(username=username, full_name=fullName, age=age, birth_date=birthDate, city=governorate, work=job, work_locations=jobAddress, number=phone, img=img, salary=monthlySalary)
-            form2.save()
-            user = authenticate(request, username=username, password=password)
-            login(request, user)
-            return render(request, "base.html")
-        except:
-            return render(request, 'signup_sponsor.html', {'msg': 'This username has already been used'})
-    return render(request, 'signup_sponsor.html')
-
-
-@requires_csrf_token
-def logins(request):
-        if request.method == "POST":
-            username = request.POST.get('username')
-            password = request.POST.get('password')
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return render(request, 'base.html', {'user': username})
-        return render(request, 'login.html')
-
-
-def logouts(request):
-    logout(request)
-
-
-@requires_csrf_token
-def signup_student(request):
-    if request.method == 'POST':
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        fullName = request.POST.get("fullName")
-        age = request.POST.get("age")
-        birthDate = request.POST.get("birthDate")
-        governorate = request.POST.get("governorate")
-        stage = request.POST.get("stage")
-        phone = request.POST.get("phone")
-        img = request.POST.get("img")
-
-        try:
-            form1 = User.objects.create_user(username=username, password=password)
-            form1.save()
-            form2 = Students(username=username, full_name=fullName, age=age, birth_date=birthDate, city=governorate, stage=stage, number=phone, img=img)
-            form2.save()
-            user = authenticate(request, username=username, password=password)
-            login(request, user)
-            return render(request, "base.html")
-        except:
-            return render(request, 'signup_student.html', {'msg': 'This username has already been used'})
-    return render(request, 'signup_student.html')
-
-
-def auth():
-    sponsor = Sponsor.objects.get(username=request.user)
-    student = Students.objects.get(username=request.user)
+def auth(request):
+    sponsor = Sponsor.objects.filter(username=request.user)
+    student = Students.objects.filter(username=request.user)
+    print(student)
+    print(sponsor)
     x = 0
     if request.user.is_authenticated and not sponsor and not student:
         x = 1  # admin
@@ -89,3 +23,85 @@ def auth():
     else:
         x = 4  # user
     return x
+
+
+@requires_csrf_token
+def signup_sponsor(request):
+    if auth(request) == 4:  # user
+        if request.method == 'POST':
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+            fullName = request.POST.get("fullName")
+            age = request.POST.get("age")
+            birthDate = request.POST.get("birthDate")
+            governorate = request.POST.get("governorate")
+            job = request.POST.get("job")
+            jobAddress = request.POST.get("jobAddress")
+            phone = request.POST.get("phone")
+            img = request.POST.get("img")
+            monthlySalary = request.POST.get("monthlySalary")
+            try:
+                form1 = User.objects.create_user(username=username, password=password)
+                form1.save()
+                form2 = Sponsor(username=username, full_name=fullName, age=age, birth_date=birthDate, city=governorate, work=job, work_locations=jobAddress, number=phone, img=img, salary=monthlySalary)
+                form2.save()
+                user = authenticate(request, username=username, password=password)
+                login(request, user)
+                return render(request, "base.html")
+            except:
+                return render(request, 'signup_sponsor.html', {'msg': 'This username has already been used'})
+        return render(request, 'signup_sponsor.html')
+    else:
+        return render(request, 'login.html')
+
+
+@requires_csrf_token
+def logins(request):
+    if auth(request) == 4:  # user
+        if request.method == "POST":
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return render(request, 'base.html', {'user': username})
+        return render(request, 'login.html')
+    else:
+        return render(request, 'base.html')
+
+
+def logouts(request):
+    if not auth(request) == 4:  # not user
+        logout(request)
+    else:
+        return render(request, 'base.html')
+
+
+@requires_csrf_token
+def signup_student(request):
+    if auth(request) == 4:  # user
+        if request.method == 'POST':
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+            fullName = request.POST.get("fullName")
+            age = request.POST.get("age")
+            birthDate = request.POST.get("birthDate")
+            governorate = request.POST.get("governorate")
+            stage = request.POST.get("stage")
+            phone = request.POST.get("phone")
+            img = request.POST.get("img")
+
+            try:
+                form1 = User.objects.create_user(username=username, password=password)
+                form1.save()
+                form2 = Students(username=username, full_name=fullName, age=age, birth_date=birthDate, city=governorate, stage=stage, number=phone, img=img)
+                form2.save()
+                user = authenticate(request, username=username, password=password)
+                login(request, user)
+                return render(request, "base.html")
+            except:
+                return render(request, 'signup_student.html', {'msg': 'This username has already been used'})
+        return render(request, 'signup_student.html')
+
+    else:
+        return render(request, 'login.html')
