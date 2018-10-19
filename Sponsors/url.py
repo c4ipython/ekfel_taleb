b90a99a3804a0c25a629a4593c102fa9148bb427
randmustafa9 <rand.mustaffa@codeforiraq.org>
2018-10-19 12:08:05 +0300

    edit something in sponsor/url.py file

--- Sponsors/url.py
+++ Sponsors/url.py
@@ -5,10 +5,10 @@ from Sponsors import views
 
 urlpatterns = [
 
-path('kafalat/req/<int:id>', views.sponserRequest, name='6alab'),
+path('kafalat/req/<int:id>', views.sponserRequest, name='kafalatRequest'),
 path('kafalat/', views.displayKafalat, name='kafalat'),
 path('kafalaty/', views.displayMyKafalat, name='kafalaty'),
-path('kafalaty/del/<int:id>', views.sponserdelete, name='delete'),
+path('kafalaty/del/<int:id>', views.sponserdelete, name='kafalatyDelete'),
 path('admin/req/<int:id>', views.reqAdmin, name='reqAdmin'),
 path('admin/del/<int:id>', views.delAdmin, name='delAdmin'),
 
