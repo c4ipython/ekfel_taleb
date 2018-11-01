ca2d9a8cd2425f62798838b7f800f490ee68b5b9
sherinali <seagull_jonthan@yahoo.co.uk>
2018-11-01 12:19:54 +0300

    add some change to home app pages

--- home/forms.py
+++ home/forms.py
@@ -3,6 +3,6 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    email = forms.EmailField(required=True)
-    name = forms.CharField(required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
+    email = forms.EmailField(required=True, label='الايميل')
+    name = forms.CharField(required=True, label='الاسم')
+    message = forms.CharField(widget=forms.Textarea, required=True, label='الرسالة')
