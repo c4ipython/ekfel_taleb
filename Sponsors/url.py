482a4c83a3b8fc1e20f4098c78caba72db2eb364
randmustafa9 <rand.mustaffa@codeforiraq.org>
2019-05-08 16:59:39 +0300

    add admin (sponser control page)

--- Sponsors/url.py
+++ Sponsors/url.py
@@ -4,15 +4,13 @@ from Sponsors import views
 
 
 urlpatterns = [
-
-path('kafalat/req/<int:id>', views.sponserRequest, name='kafalatRequest'),
+path('adminKafalat/', views.adminDisplayKafalatListView.as_view(), name='adminKafalat'),
+path('kafalat/', views.displayKafalatListView.as_view(), name='kafalat'),
+path('kafalaty/', views.displayMyKafalatListView.as_view(), name='kafalaty'),
+path('kafalat/req/<int:id>', views.addSponserRequest, name='kafalatRequest'),
 path('kafalaty/del/<int:id>', views.sponserdelete, name='kafalatyDelete'),
-path('kafalat/', views.displayKafalat, name='kafalat'),
-path('kafalaty/', views.displayMyKafalat, name='kafalaty'),
-path('kafalat/adminA/<int:id>', views.acceptAdmin, name='adminA'),
-path('kafalat/adminb/<int:id>', views.refuseAdmin, name='adminb'),
-path('kafalat/admind/<int:id>', views.delAdmin, name='admind'),
-path('kafalat/adminRA/<int:id>', views.adminReq_stA, name='adminRA'),
-path('kafalat/adminRB/<int:id>', views.adminReq_stB, name='adminRB'),
+path('kafalat/adminA/<int:id>', views.adminAncceptTheSponserRequest, name='adminA'),
+path('kafalat/adminb/<int:id>', views.adminRefuseTheSponserRequest, name='adminb'),
+path('kafalat/admind/<int:id>', views.adminDeleteTheSponser, name='admind')
 
 ]
